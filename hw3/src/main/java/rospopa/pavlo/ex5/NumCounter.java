7bf5d44263039810b42ba133f69128da15c551c2
Pavlo Rospopa <rospopa.pavlo@gmail.com>
2023-03-05 21:12:34 +0200

    hw3: ex5 solution

---
+++ hw3/src/main/java/rospopa/pavlo/ex5/NumCounter.java
@@ -0,0 +1,54 @@
+package rospopa.pavlo.ex5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumCounter {
+    final int[] numCounters;
+
+    NumCounter(int n) {
+        this.numCounters = new int[n];
+    }
+
+    int get(int num) {
+        return numCounters[num];
+    }
+
+    int increment(int num) {
+        return ++numCounters[num];
+    }
+
+    int decrement(int num) {
+        return --numCounters[num];
+    }
+
+    public static void main(String[] args) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+
+        var input = reader.readLine().split(" ");
+        var n = Integer.parseInt(input[0]);
+        var k = Integer.parseInt(input[1]);
+
+        input = reader.readLine().split(" ");
+        var arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        var l = 0;
+        var subarrayCounter = 0;
+        var counter = new NumCounter(n);
+        for (int r = 0; r < n; r++) {
+            var rNum = arr[r];
+            if (counter.increment(rNum) >= k) {
+                subarrayCounter += n - r;
+                while (l < n - 1 && (counter.decrement(arr[l++]) >= k || counter.get(rNum) >= k)) {
+                    subarrayCounter += n - r;
+                }
+            }
+        }
+
+        System.out.println(subarrayCounter);
+    }
+}
