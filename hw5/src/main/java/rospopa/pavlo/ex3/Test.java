3e2293927b4d28427a2b9edf1f450e3c032c05de
Pavlo Rospopa <rospopa.pavlo@gmail.com>
2023-03-26 22:18:13 +0300

    hw5: ex3 test

---
+++ hw5/src/main/java/rospopa/pavlo/ex3/Test.java
@@ -0,0 +1,17 @@
+package rospopa.pavlo.ex3;
+
+public class Test {
+
+    public static void main(String[] args) {
+        var deque = new MyCircularDeque(3);
+        System.out.println(deque.insertLast(1));
+        System.out.println(deque.insertLast(2));
+        System.out.println(deque.insertFront(3));
+        System.out.println(deque.insertFront(4));
+        System.out.println(deque.getRear());
+        System.out.println(deque.isFull());
+        System.out.println(deque.deleteLast());
+        System.out.println(deque.insertFront(4));
+        System.out.println(deque.getFront());
+    }
+}
