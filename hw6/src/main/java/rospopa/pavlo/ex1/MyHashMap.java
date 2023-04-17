6271453c96aa4aba8b0432dbbf97e444f8a7b0a8
Pavlo Rospopa <rospopa.pavlo@gmail.com>
2023-04-18 00:49:32 +0300

    hw6: re-implement MyHashMap to allocate max needed memory during its creation (passes leet code tests)

--- hw6/src/main/java/rospopa/pavlo/ex1/MyHashMap.java
+++ hw6/src/main/java/rospopa/pavlo/ex1/MyHashMap.java
@@ -1,28 +1,20 @@
 package rospopa.pavlo.ex1;
 
 class MyHashMap {
-    private final double resizeFactor;
-    private final double loadFactor;
     private final int a;
     private final int b;
-    private Entry[] arr;
+    private final Entry[] arr;
+    private final int capacity;
     private int length;
-    private int capacity;
 
     public MyHashMap() {
-        resizeFactor = 1.5;
-        loadFactor = 0.75;
-        capacity = 10;
+        capacity = 53334;
         arr = new Entry[capacity];
         a = 1009;
         b = 9973;
     }
 
     public void put(int key, int value) {
-        if (getLength() + 1.0 / capacity > loadFactor) {
-            increaseCapacity();
-        }
-
         var index = hash(key) % capacity;
         var head = arr[index];
         var entry = findEntry(key, head);
@@ -68,39 +60,8 @@ class MyHashMap {
         length--;
     }
 
-    int getLength() {
-        return length;
-    }
-
     private int hash(int key) {
         return a * key + b;
-    }
-
-    private void increaseCapacity() {
-        capacity = (int) (capacity * resizeFactor);
-        var newArr = new Entry[capacity];
-        for (Entry head : arr) {
-            if (head == null) {
-                continue;
-            }
-
-            var index = hash(head.key) % capacity;
-            newArr[index] = join(newArr[index], head);
-        }
-        arr = newArr;
-    }
-
-    private Entry join(Entry head, Entry tail) {
-        if (head == null) {
-            return tail;
-        }
-
-        var entry = head;
-        while (entry.next != null) {
-            entry = entry.next;
-        }
-        entry.next = tail;
-        return head;
     }
 
     static class Entry {
